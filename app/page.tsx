3d3febd35bde791f9d7764853863773fa9b0aa7a
Aygiz Khatipov <you@example.com>
2024-10-28 22:45:53 +0300

    dadsdsasd

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import Image from 'next/image';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+
 import { lusitana } from './ui/fonts';
 
 export default function Page() {
